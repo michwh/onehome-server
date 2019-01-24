1fce27f13e1d94cd56c1b93df0726c7f642f9b2f
michwh <fjsxhwh@163.com>
2019-01-24 16:01:53 +0800

    完成我的收藏功能

--- collection/urls.py
+++ collection/urls.py
@@ -6,4 +6,5 @@ app_name = 'collection'
 
 urlpatterns = [
     url(r'changeCollectState', CollectStateViewset.as_view({'post': 'post'})),
+    url(r'getCollectionList', CollectionListViewset.as_view({'get': 'get'})),
 ]
--- collection/views.py
+++ collection/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from collection.serializers import *
+from product.views import sort_out_list
+from product.serializers import *
 
 
 # 改变收藏状态
@@ -43,3 +45,23 @@ class CollectStateViewset(viewsets.ModelViewSet):
                 'msg': '没有操作权限'
             }
             return Response(obj, 201)
+
+
+# 获取收藏列表
+class CollectionListViewset(viewsets.ModelViewSet):
+    def get(self, request):
+        if request.user.is_authenticated:
+            try:
+                product_list = Product.objects.filter(collection__username=str(request.user))
+            except Collection.DoesNotExist:
+                product_list = None
+            ser = ProductSerializer(instance=product_list, many=True)
+            msg = sort_out_list(request, ser.data)
+            return Response(msg, status=HTTP_200_OK)
+        else:
+            obj = {
+                'stateCode': 201,
+                'msg': '没有操作权限'
+            }
+            return Response(obj, 201)
+
